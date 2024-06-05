820d864c3281f94e2d9e9da0c1364d8f6c529f8c
Diyaridem <netdiy32@hotmail.com>
2024-06-05 23:39:00 +0300

    CategoryCard Design

--- src/screens/CategoryCard.js
+++ src/screens/CategoryCard.js
@@ -4,18 +4,29 @@ import { Card, CardContent, Typography, CardMedia } from '@mui/material';
 function CategoryCard({ category }) {
   return (
     <div style={styles.cardContainer}>
-      <Card sx={{ minWidth: 100, maxWidth: 300, backgroundColor: '#f0f0f0' }} >
+      <Card sx={{ 
+          minWidth: 100, 
+          maxWidth: 300, 
+          backgroundColor: '#f0f0f0', 
+          borderRadius: '15px', 
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', 
+          transition: 'box-shadow 0.3s ease-in-out',
+          '&:hover': {
+            boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)'
+          } 
+        }} 
+      >
         <CardMedia
           component="img"
           height="140"
           image={category.imageUrl}
           alt={category.title}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent style={styles.cardContent}>
+          <Typography gutterBottom variant="h5" component="div" style={styles.title}>
             {category.title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body2" color="text.secondary" style={styles.description}>
             {category.description}
           </Typography>
         </CardContent>
@@ -28,9 +39,21 @@ const styles = {
   cardContainer: {
     flex: 1,
     padding: 24,
-    position:'relative',
-    marginBottom:'30px'
-  
+    position: 'relative',
+    marginBottom: '30px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardContent: {
+    textAlign: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  description: {
+    color: '#777'
   }
 };
 
